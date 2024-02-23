e54537ecceff68d32b50eb4b3d39878727dfb027
Bars <Bars@HOME-PC>
2024-02-23 09:54:57 +0300

    Добавьте файлы проекта.

---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public int k = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //protected override void OnAppearing()
+        //{
+        //    StackLayout stack = new StackLayout();
+
+        //    Label label = new Label();
+        //    label.Text = "govno2";
+        //    label.HorizontalOptions = LayoutOptions.Center;
+        //    label.VerticalOptions = LayoutOptions.Center;
+
+        //    Content = stack;
+        //}
+
+        //private void Button_Clicked(object sender, EventArgs e)
+        //{
+        //    k++;
+        //    if (k % 2 == 0)
+        //    {
+        //        ee.Text = k.ToString();
+        //    }
+        //    else { ee.Text = "Govno";  }
+        //}
+
+       
+    }
+}
